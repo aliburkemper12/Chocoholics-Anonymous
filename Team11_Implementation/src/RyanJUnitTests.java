1f1250a4000e384b62a95ebcbe3e1fe2a64abad7
ryankutella25 <ryankutella25@yahoo.com>
2023-11-02 12:53:09 -0500

    Added JUnit file

---
+++ Team11_Implementation/src/RyanJUnitTests.java
@@ -0,0 +1,32 @@
+import org.junit.After;
+import  org.junit.Before;
+import org.junit.Test;
+
+public class RyanJUnitTests {
+    
+    @Before
+    public void setUp(){}
+
+    @Test
+    public void failureTest1(){
+        
+    }
+
+    @Test
+    public void failureTest2(){
+        
+    }
+
+    @Test
+    public void successTest1(){
+        
+    }
+
+    @Test
+    public void successTest2(){
+        
+    }
+
+    @After
+    public void notNeeded(){}
+}
