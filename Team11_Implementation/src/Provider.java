ceab8d71cf22efac42077b00624751905644d2bb
ryankutella25 <ryankutella25@yahoo.com>
2023-11-05 17:16:14 -0600

    typo

--- Team11_Implementation/src/Provider.java
+++ Team11_Implementation/src/Provider.java
@@ -1,8 +1,8 @@
-import java.util.arrayList;
+import java.util.ArrayList;
 public class Provider{
     private String name;
     private int credentials;
-    private arrayList<Service> services;
+    private ArrayList<Service> services;
     Provider(String name, int credentials){
         this.name = name;
         this.credentials = credentials;
@@ -16,7 +16,7 @@ public class Provider{
     public int getCreds(){
         return credentials;
     }
-    public arrayList<Service> getServices(){
+    public ArrayList<Service> getServices(){
         return services;
     }
 }
