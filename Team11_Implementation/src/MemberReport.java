6b1cd0614655ca65572f577d12f1e9168800de96
aeburkemper <aeburkemper@crimson.ua.edu>
2023-11-05 13:45:08 -0600

    added member report

---
+++ Team11_Implementation/src/MemberReport.java
@@ -0,0 +1,13 @@
+// Class containing information for member report.
+
+// @author Ali Burkemper
+
+public class MemberReport {
+    String service;
+    String providerName;
+
+    // get all information for member and fill it in
+    public MemberReport (String number) {
+        // do stuff
+    }
+}
