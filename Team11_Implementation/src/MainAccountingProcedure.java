6d4bc3133e0b3f381ae6a57bb264892e25ef9a32
JackSherry6 <122834592+JackSherry6@users.noreply.github.com>
2023-11-05 18:11:53 -0600

    updates to my classes

---
+++ Team11_Implementation/src/MainAccountingProcedure.java
@@ -0,0 +1,14 @@
+//@author Jack Sherry
+
+public class MainAccountingProcedure {
+    
+    // prompt the creation of a new report when prompted by timer or prompted from manager terminal
+
+    public void summaryReport(){
+
+    }
+
+    public void EFTReport(){
+
+    }
+}
--- Team11_Implementation/src/ServiceRecord.java
+++ Team11_Implementation/src/ServiceRecord.java
@@ -4,7 +4,67 @@ import java.util.List;
 
 public class ServiceRecord {
 
-    String date;
-    String dateService;
-    int providerNum;
+    private String date = "";
+    private String dateService = "";
+    private int providerNum = 0;
+    private int memberNum = 0;
+    private int serviceCode = 0;
+    private String comment = "";
+
+    public ServiceRecord(String date, String dateService, int providerNum, int memberNum, int serviceCode, String comment){
+        this.date = date;
+        this.dateService = dateService;
+        this.providerNum = providerNum;
+        this.memberNum = memberNum;
+        this.serviceCode = serviceCode;
+        this.comment = comment;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public void setDateService(String dateService) {
+        this.dateService = dateService;
+    }
+
+    public void setMemberNum(int memberNum) {
+        this.memberNum = memberNum;
+    }
+
+    public void setProviderNum(int providerNum) {
+        this.providerNum = providerNum;
+    }
+
+    public void setServiceCode(int serviceCode) {
+        this.serviceCode = serviceCode;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public String getDateService() {
+        return dateService;
+    }
+
+    public int getProviderNum() {
+        return providerNum;
+    }
+
+    public int getMemberNum() {
+        return memberNum;
+    }
+
+    public int getServiceCode() {
+        return serviceCode;
+    }
+
+    public String getComment() {
+        return comment;
+    }
 }
