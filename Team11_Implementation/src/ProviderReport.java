e65d0279ae20204f2d100d20eb2653e832fd03ea
ldjontz <ldjontz@crimson.ua.edu>
2023-11-08 15:37:48 -0600

    update ProviderReport

--- Team11_Implementation/src/ProviderReport.java
+++ Team11_Implementation/src/ProviderReport.java
@@ -4,8 +4,10 @@ public class ProviderReport {
     ArrayList<DiskRecord> records;
     int weekFee;
     String memberName;
+    String report;
 
     ProviderReport(){
+        weekFee = 0;
     }
 
     public void addRecord(DiskRecord record){
@@ -15,10 +17,39 @@ public class ProviderReport {
     public String writeReport(){
         String currDate;
         String serviceDate;
-        
+        Long memberNum;
+        int serviceCode;
+        int fee;
+        int consultations = 0;
         
         for(DiskRecord record : records){
-
+            currDate = record.getCurrDate();
+            serviceDate = record.serviceDate();
+            memberNum = record.getMemberNum();
+            serviceCode = record.getServiceCode();
+            fee = record.getFee();
+            report += "Current date and time: ";
+            report += currDate;
+            report += "\nService date: ";
+            report += serviceDate;
+            report += "\nMember name and number: ";
+            memberName = 
+            report += memberName;
+            report += " ";
+            report += memberNum;
+            report += "\nService code: ";
+            report += serviceCode;
+            report += "\nFee: ";
+            report += fee;
+            report += "\n\n"
+            consultations++;
+            weekFee += fee;
         }
+        report += "Number of consultations: ";
+        report += consultations;
+        report += "\nTotal fee for the week: ";
+        report += fee;
+        report += "\n";
+        return report;
     }
 }
