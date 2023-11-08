59d6d8fd07d4839ce105dfbf1bccf203c3b16104
JackSherry6 <122834592+JackSherry6@users.noreply.github.com>
2023-11-08 17:22:21 -0600

    mainaccountingprocedure

--- Team11_Implementation/src/MainAccountingProcedure.java
+++ Team11_Implementation/src/MainAccountingProcedure.java
@@ -1,8 +1,30 @@
+import java.util.ArrayList;
+
 //@author Jack Sherry
 
 public class MainAccountingProcedure {
     
+    AllMembers members;
+    AllProviders providers;
+
+    MainAccountingProcedure(AllMembers members, AllProviders providers){
+        this.members = members;
+        this.providers = providers;
+    }
+
     public void runMainAccountingProcedure(){
+
+        //getmemberlist
+
+        ArrayList<Member> memberList = members.memberList;
+        ArrayList<Provider> providerList = providers.providerList;
+
+        public boolean findMember(int memberNumber){
+            for (int i = 0; i < members.size(); i++) {
+                Member toFindMember = memberList.get(i);
+            }
+        }
+
         // request report is called and members and stuff are passed in
         Report newReport = new Report();
         newReport.generateProviderReport();
