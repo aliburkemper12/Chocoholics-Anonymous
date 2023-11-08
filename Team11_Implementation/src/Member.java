2cbbc1504acece07372ebcab2f4e04ecb5a7347c
aeburkemper <aeburkemper@crimson.ua.edu>
2023-11-08 14:34:35 -0600

    added constructors

--- Team11_Implementation/src/Member.java
+++ Team11_Implementation/src/Member.java
@@ -7,42 +7,23 @@ import java.util.List;
 
 // name number address city zipcode
 // pulling from array in all members, see provider
+// store data here
 public class Member {
 
     private String name;
     private int memberNumber;
-<<<<<<< HEAD
-=======
     private String status;
->>>>>>> 56beeb5a2091a17a99a6f0c66810ae778ccfb7db
+    private String address;
+    private String city;
+    private int zipcode;
+    private 
 
-    //Could do this for constructor - Adison
-    /*
-     * public Member(String name, int memberNumber) {
-     *   this.name = name;
-     *   this.memberNumber = memberNumber;
-     * }
-     * 
-     * public int getMemberNumber() {
-     *   return memberNumber;
-     * }
-     */
-
-    // returns member ID number from database by searching for 
-    // name [CSV files?] 
-    public Member getMemberNumber(String name) {
-        List<Member> members = getMemberInfo();
-        for (Member member : members) {
-            if(member.getName().equals(name))
-                return member;
-        }
-        return null;
+    public int getMemberNumber() {
+        return memberNumber;
     }
-    
-    // returns list of members and their info after searching database
-    public List<Member> getMemberInfo() {
-        // Do stuff
-        return null;
+
+    public void setMemberNumber(Int memberNumber) {
+        this.memberNumber = memberNumber;
     }
 
     public  String getName() {
@@ -52,5 +33,36 @@ public class Member {
     public void setName(String name) {
         this.name = name;
     }
+
+    public  String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public  String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
     
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public int getZip() {
+        return zipcode;
+    }
+
+    public void setZip(int zipcode) {
+        this.zipcode = zipcode;
+    }
 }
