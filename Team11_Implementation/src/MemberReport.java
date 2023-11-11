f9cf5cc1cebac019dd54f2e2612a19a6d384974b
aeburkemper <aeburkemper@crimson.ua.edu>
2023-11-10 18:26:32 -0600

    creates a file

--- Team11_Implementation/src/MemberReport.java
+++ Team11_Implementation/src/MemberReport.java
@@ -2,19 +2,58 @@
 
 // @author Ali Burkemper
 
+// passes in member to generate member report
+
+import java.util.ArrayList;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.time.LocalDate;
+
 public class MemberReport {
+    ArrayList<Member> memberList;
     String service;
     String providerName;
     long number;
+    String member_report;
 
-    // get all information for member and fill it in.. also generate?
-    public MemberReport (String service, String providerName) {
-        this.service = service;
-        this.providerName = providerName;
-    }
+    public void generateReport(Member member){
+        String memberName;
+        String memberAddress;
+        String memberCity;
+        String memberState;
+        int memberZip;
+        long memberNum;
 
-    public void generateMemberReport(long number){
-        
+        // String serviceDate;
+        // int serviceCode;
+        // String providerName;
+
+        ArrayList<ServiceRecord> sRecords = member.getService();
+        String service = sRecords.toString();
+
+        memberName = member.getName();
+        memberNum = member.getMemberNumber();
+        memberAddress = member.getAddress();
+        memberCity = member.getCity();
+        memberState = member.getState();
+        memberZip = member.getZip();
+        // currDate = sRecords.getDate();
+        member_report += memberName + "\n" + memberNum + "\n" + memberAddress + "\n" + memberCity + "\n" + memberState + "\n" + memberZip + "\n" + service + "\n";
+        LocalDate currDate = LocalDate.now();
+
+
+        // Create file
+        File outputFile = new File("../data/" + memberName + currDate + ".txt");
+        try{
+            FileWriter myWriter = new FileWriter("../data/" + memberName + currDate + ".txt");
+            myWriter.write(member_report);
+            myWriter.close();
+        } catch(IOException e){
+            System.out.println("error occurred in member report file creation\n");
+        };
+
+
     }
 
 }
