b78b799e2c4e13a04a701a3d1e8b7b739340fe70
ldjontz <ldjontz@crimson.ua.edu>
2023-11-14 17:56:00 -0600

    fix LucaJUnitTests

--- Team11_Implementation/src/LucaJUnitTests.java
+++ Team11_Implementation/src/LucaJUnitTests.java
@@ -3,7 +3,7 @@ import static org.junit.Assert.*;
 import org.junit.After;
 import  org.junit.Before;
 import org.junit.Test;
-import.java.util.ArrayList;
+import java.util.ArrayList;
 
 //Class for testing
 //Method testProviderGetName() and testProviderDirectoryRequestServices() are from my classes
